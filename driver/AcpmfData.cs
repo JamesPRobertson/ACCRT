2606ff03fc14f9ed5b53bf0bdae25bbd1ee877b4
JamesPRobertson <jamespr.0312@gmail.com>
2022-06-29 00:39:26 -0500

    Commented out broken struct

--- driver/AcpmfData.cs
+++ driver/AcpmfData.cs
@@ -62,96 +62,96 @@ namespace AcpmfData {
       public SPageFileGraphic() {}
    };
 
-   struct SPageFilePhysics
-{
+   /*
+   struct SPageFilePhysics {
+      int packetId = 0;
+      float gas = 0;
+      float brake = 0;
+      float fuel = 0;
+      int gear = 0;
+      int rpms = 0;
+      float steerAngle = 0;
+      float speedKmh = 0;
+      float velocity[3];
+      float accG[3];
+      float wheelSlip[4];
+      float wheelLoad[4];
+      float wheelsPressure[4];
+      float wheelAngularSpeed[4];
+      float tyreWear[4];
+      float tyreDirtyLevel[4];
+      float tyreCoreTemperature[4];
+      float camberRAD[4];
+      float suspensionTravel[4];
+      float drs = 0;
+      float tc = 0;
+      float heading = 0;
+      float pitch = 0;
+      float roll = 0;
+      float cgHeight;
+      float carDamage[5];
+      int numberOfTyresOut = 0;
+      int pitLimiterOn = 0;
+      float abs = 0;
+      float kersCharge = 0;
+      float kersInput = 0;
+      int autoShifterOn = 0;
+      float rideHeight[2];
+      float turboBoost = 0;
+      float ballast = 0;
+      float airDensity = 0;
+      float airTemp = 0;
+      float roadTemp = 0;
+      float localAngularVel[3];
+      float finalFF = 0;
+      float performanceMeter = 0;
 
-    int packetId = 0;
-    float gas = 0;
-    float brake = 0;
-    float fuel = 0;
-    int gear = 0;
-    int rpms = 0;
-    float steerAngle = 0;
-    float speedKmh = 0;
-    float velocity[3];
-    float accG[] = new float[3];
-    float wheelSlip[4];
-    float wheelLoad[4];
-    float wheelsPressure[4];
-    float wheelAngularSpeed[4];
-    float tyreWear[4];
-    float tyreDirtyLevel[4];
-    float tyreCoreTemperature[4];
-    float camberRAD[4];
-    float suspensionTravel[4];
-    float drs = 0;
-    float tc = 0;
-    float heading = 0;
-    float pitch = 0;
-    float roll = 0;
-    float cgHeight;
-    float carDamage[5];
-    int numberOfTyresOut = 0;
-    int pitLimiterOn = 0;
-    float abs = 0;
-    float kersCharge = 0;
-    float kersInput = 0;
-    int autoShifterOn = 0;
-    float rideHeight[2];
-    float turboBoost = 0;
-    float ballast = 0;
-    float airDensity = 0;
-    float airTemp = 0;
-    float roadTemp = 0;
-    float localAngularVel[3];
-    float finalFF = 0;
-    float performanceMeter = 0;
+      int engineBrake = 0;
+      int ersRecoveryLevel = 0;
+      int ersPowerLevel = 0;
+      int ersHeatCharging = 0;
+      int ersIsCharging = 0;
+      float kersCurrentKJ = 0;
 
-    int engineBrake = 0;
-    int ersRecoveryLevel = 0;
-    int ersPowerLevel = 0;
-    int ersHeatCharging = 0;
-    int ersIsCharging = 0;
-    float kersCurrentKJ = 0;
+      int drsAvailable = 0;
+      int drsEnabled = 0;
 
-    int drsAvailable = 0;
-    int drsEnabled = 0;
+      float brakeTemp[4];
+      float clutch = 0;
 
-    float brakeTemp[4];
-    float clutch = 0;
+      float tyreTempI[4];
+      float tyreTempM[4];
+      float tyreTempO[4];
 
-    float tyreTempI[4];
-    float tyreTempM[4];
-    float tyreTempO[4];
+      int isAIControlled;
 
-    int isAIControlled;
+      float tyreContactPoint[4][3];
+      float tyreContactNormal[4][3];
+      float tyreContactHeading[4][3];
 
-    float tyreContactPoint[4][3];
-    float tyreContactNormal[4][3];
-    float tyreContactHeading[4][3];
+      float brakeBias = 0;
 
-    float brakeBias = 0;
+      float localVelocity[3];
 
-    float localVelocity[3];
+      int P2PActivations = 0;
+      int P2PStatus = 0;
 
-    int P2PActivations = 0;
-    int P2PStatus = 0;
+      int currentMaxRpm = 0;
 
-    int currentMaxRpm = 0;
-
-    float mz[4];
-    float fx[4];
-    float fy[4];
-    float slipRatio[4];
-    float slipAngle[4];
+      float mz[4];
+      float fx[4];
+      float fy[4];
+      float slipRatio[4];
+      float slipAngle[4];
 
 
-    int tcinAction = 0;
-    int absInAction = 0;
-    float suspensionDamage[4];
-    float tyreTemp[4];
+      int tcinAction = 0;
+      int absInAction = 0;
+      float suspensionDamage[4];
+      float tyreTemp[4];
 
-};
+   };
+   */
 
    struct SPageFileStatic {
       string smVersion = "";
