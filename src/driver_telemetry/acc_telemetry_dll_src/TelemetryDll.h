eea2bf17a4620c9fe883dc4815a2a6af8eea2d84
JamesPRobertson <jamespr.0312@gmail.com>
2022-07-04 15:34:41 -0500

    Reorganized project

---
+++ src/driver_telemetry/acc_telemetry_dll_src/TelemetryDll.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "pch.h"
+#include "SharedFileOut.h"
+
+#ifndef CPPWIN32DLL_EXPORTS
+#define CPPWIN32DLL_EXPORTS __declspec(dllexport)
+#endif
+
+extern "C" {
+    CPPWIN32DLL_EXPORTS struct SPageFilePhysics;
+    CPPWIN32DLL_EXPORTS struct SPageFileGraphics;
+    CPPWIN32DLL_EXPORTS struct SPageFileStatic;
+
+    CPPWIN32DLL_EXPORTS void update_physics_struct (OUT SPageFilePhysics*  ptr_phys_struct);
+    CPPWIN32DLL_EXPORTS void update_graphics_struct(OUT SPageFileGraphics* ptr_grph_struct);
+    CPPWIN32DLL_EXPORTS void update_static_struct  (OUT SPageFileStatic*   ptr_stc_struct);
+}
+
