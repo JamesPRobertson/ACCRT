eba7a5cc2a429b987e6513571f0dc36fac548e50
Justin Pierre <Justin@JPierre.pro>
2022-07-10 14:58:21 -0500

    Fixed issues from Roslynator

--- src/driver_telemetry/DriverTelemetry.cs
+++ src/driver_telemetry/DriverTelemetry.cs
@@ -3,132 +3,139 @@ using Server;
 using System.Runtime.InteropServices;
 using System.Text.Json;
 
-namespace Driver {
-   class TelemetryParser {
-      SPageFilePhysics  physics_info;
-      SPageFileGraphics graphics_info;
-      SPageFileStatic   static_info;
+namespace Driver
+{
+    class TelemetryParser
+    {
+        SPageFilePhysics physics_info;
+        SPageFileGraphics graphics_info;
+        SPageFileStatic static_info;
 
-      IntPtr ptr_physics;
-      IntPtr ptr_graphics;
-      IntPtr ptr_static;
+        IntPtr ptr_physics;
+        IntPtr ptr_graphics;
+        IntPtr ptr_static;
 
-      const string DLL_DATA_FILE = @".\acc_telemetry.dll";
+        const string DLL_DATA_FILE = @".\acc_telemetry.dll";
 
-      public TelemetryParser() {
-         InitializeTelemetry();
-      }
+        public TelemetryParser()
+        {
+            InitializeTelemetry();
+        }
 
-      ~TelemetryParser() {
-         Marshal.FreeHGlobal(ptr_physics);
-         Marshal.FreeHGlobal(ptr_graphics);
-         Marshal.FreeHGlobal(ptr_static);
-         ptr_physics  = IntPtr.Zero;
-         ptr_graphics = IntPtr.Zero;
-         ptr_static   = IntPtr.Zero;
-      }
+        ~TelemetryParser()
+        {
+            Marshal.FreeHGlobal(ptr_physics);
+            Marshal.FreeHGlobal(ptr_graphics);
+            Marshal.FreeHGlobal(ptr_static);
+            ptr_physics = IntPtr.Zero;
+            ptr_graphics = IntPtr.Zero;
+            ptr_static = IntPtr.Zero;
+        }
 
-      public void TestPrintData() {
-         Console.WriteLine(String.Join("\n", this.GetStringTelemetryData()));
-      }
+        public void TestPrintData()
+        {
+            Console.WriteLine(String.Join("\n", this.GetStringTelemetryData()));
+        }
 
-      public string GetJSONTelemetryData() {
-         this.UpdateAllTelemetrySources();
+        public string GetJSONTelemetryData()
+        {
+            this.UpdateAllTelemetrySources();
 
-         JsonSerializerOptions json_options = new JsonSerializerOptions();
-         // For human readability in debug
-         //json_options.WriteIndented = true;
-         json_options.IncludeFields = true;
+            JsonSerializerOptions json_options = new();
+            // For human readability in debug
+            //json_options.WriteIndented = true;
+            json_options.IncludeFields = true;
 
-         ACCSharedMemoryDefinitionsPack packed_data = new ACCSharedMemoryDefinitionsPack();
-         packed_data.graphics_data = this.graphics_info;
-         packed_data.physics_data = this.physics_info;
-         packed_data.static_data = this.static_info;
+            return JsonSerializer.Serialize<ACCSharedMemoryDefinitionsPack>(new(this.graphics_info, this.physics_info, this.static_info), json_options);
+        }
 
-         return JsonSerializer.Serialize<ACCSharedMemoryDefinitionsPack>(packed_data, json_options);
-      }
+        public List<string> GetStringTelemetryData()
+        {
+            UpdateAllTelemetrySources();
 
-      public List<string> GetStringTelemetryData() {
-         UpdateAllTelemetrySources();
+            List<string> data = new();
 
-         List<string> data = new List<string>();
+            data.Add("----------------------------------------------\n");
+            data.Add($"Current Packet: {this.physics_info.packetId}");
+            data.Add("Temperatures:");
+            data.Add($"   Air:      {this.physics_info.airTemp:00} c");
+            data.Add($"   Track:    {this.physics_info.roadTemp:00} c");
+            data.Add("\n");
 
-         data.Add("----------------------------------------------\n");
-         data.Add($"Current Packet: {this.physics_info.packetId}");
-         data.Add("Temperatures:");
-         data.Add($"   Air:      {this.physics_info.airTemp:00} c");
-         data.Add($"   Track:    {this.physics_info.roadTemp:00} c");
-         data.Add("\n");
-         
-         data.Add($"speed:       {this.physics_info.speedKmh:000.0} km/h");
-         data.Add($"throttle:    {this.physics_info.gas:0.00%}");
-         data.Add($"brake:       {this.physics_info.brake:0.00%}");
-         data.Add($"RPM:         {this.physics_info.rpms}");
-         data.Add($"gear:        {this.physics_info.gear}");
-         data.Add($"fuel:        {this.physics_info.fuel:0.00} L");
-         data.Add($"fuel / lap:  {this.graphics_info.fuelXLap:0.00} L");
-         data.Add("\n");
+            data.Add($"speed:       {this.physics_info.speedKmh:000.0} km/h");
+            data.Add($"throttle:    {this.physics_info.gas:0.00%}");
+            data.Add($"brake:       {this.physics_info.brake:0.00%}");
+            data.Add($"RPM:         {this.physics_info.rpms}");
+            data.Add($"gear:        {this.physics_info.gear}");
+            data.Add($"fuel:        {this.physics_info.fuel:0.00} L");
+            data.Add($"fuel / lap:  {this.graphics_info.fuelXLap:0.00} L");
+            data.Add("\n");
 
-         data.Add("-------------------------------\n");
-         data.Add($"Lap Time:    {this.graphics_info.currentTime}");
-         data.Add($"Last Lap:    {this.graphics_info.lastTime}");
-         data.Add($"Best Lap:    {this.graphics_info.bestTime}");
-         data.Add("\n");
+            data.Add("-------------------------------\n");
+            data.Add($"Lap Time:    {this.graphics_info.currentTime}");
+            data.Add($"Last Lap:    {this.graphics_info.lastTime}");
+            data.Add($"Best Lap:    {this.graphics_info.bestTime}");
+            data.Add("\n");
 
-         data.Add($"Current Track:  {this.static_info.track}");
-         data.Add("\n");
+            data.Add($"Current Track:  {this.static_info.track}");
+            data.Add("\n");
 
-         return data;
-      }
+            return data;
+        }
 
-      void InitializeTelemetry() {
-         this.physics_info = new SPageFilePhysics();
-         this.ptr_physics = Marshal.AllocHGlobal(Marshal.SizeOf(physics_info));
+        void InitializeTelemetry()
+        {
+            this.physics_info = new SPageFilePhysics();
+            this.ptr_physics = Marshal.AllocHGlobal(Marshal.SizeOf(physics_info));
 
-         this.graphics_info = new SPageFileGraphics();
-         this.ptr_graphics = Marshal.AllocHGlobal(Marshal.SizeOf(graphics_info));
+            this.graphics_info = new SPageFileGraphics();
+            this.ptr_graphics = Marshal.AllocHGlobal(Marshal.SizeOf(graphics_info));
 
-         this.static_info = new SPageFileStatic();
-         this.ptr_static = Marshal.AllocHGlobal(Marshal.SizeOf(static_info));
-      }
+            this.static_info = new SPageFileStatic();
+            this.ptr_static = Marshal.AllocHGlobal(Marshal.SizeOf(static_info));
+        }
 
-      void UpdateAllTelemetrySources() {
+        void UpdateAllTelemetrySources()
+        {
             // TODO: maybe update these exception messages
             if (!UpdatePhysicsData(this.ptr_physics, Marshal.SizeOf(physics_info)))
-               throw new System.Exception("Physics data corrupted");
+                throw new System.Exception("Physics data corrupted");
             this.physics_info = Marshal.PtrToStructure<SPageFilePhysics>(this.ptr_physics);
 
             if (!UpdateGraphicsData(this.ptr_graphics, Marshal.SizeOf(graphics_info)))
-               throw new System.Exception("Graphics data corrupted");
+                throw new System.Exception("Graphics data corrupted");
             this.graphics_info = Marshal.PtrToStructure<SPageFileGraphics>(ptr_graphics);
 
             if (!UpdateStaticData(this.ptr_static, Marshal.SizeOf(static_info)))
-               throw new System.Exception("Static data corrupted");
+                throw new System.Exception("Static data corrupted");
             this.static_info = Marshal.PtrToStructure<SPageFileStatic>(this.ptr_static);
-      }
+        }
 
-#region DllImports
-      [DllImport(DLL_DATA_FILE, EntryPoint="update_physics_data")]
-      static extern bool UpdatePhysicsData(IntPtr PhysicsData, int data_size);
+        #region DllImports
+        [DllImport(DLL_DATA_FILE, EntryPoint = "update_physics_data")]
+        static extern bool UpdatePhysicsData(IntPtr PhysicsData, int data_size);
 
-      [DllImport(DLL_DATA_FILE, EntryPoint="update_graphics_data")]
-      static extern bool UpdateGraphicsData(IntPtr GraphicsData, int data_size);
+        [DllImport(DLL_DATA_FILE, EntryPoint = "update_graphics_data")]
+        static extern bool UpdateGraphicsData(IntPtr GraphicsData, int data_size);
 
-      [DllImport(DLL_DATA_FILE, EntryPoint="update_static_data")]
-      static extern bool UpdateStaticData(IntPtr StaticData, int data_size);
-#endregion
-   }
+        [DllImport(DLL_DATA_FILE, EntryPoint = "update_static_data")]
+        static extern bool UpdateStaticData(IntPtr StaticData, int data_size);
+        #endregion
+    }
 
-   class CLI {
-      public static void Main(string[] args) {
-         TelemetryServer output_server = new TelemetryServer(args);
-         output_server.ExecuteUDPServer();
-      }
+    static class CLI
+    {
+        public static void Main(string[] args)
+        {
+            TelemetryServer output_server = new(args);
+            output_server.ExecuteUDPServer();
+        }
 
-      public static void CustomPrintError(string message) {
-         Console.ForegroundColor = ConsoleColor.Red;
-         Console.WriteLine(message);
-         Console.ForegroundColor = ConsoleColor.White;
-      }
-   }
+        public static void CustomPrintError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
 }
--- src/driver_telemetry/Server.cs
+++ src/driver_telemetry/Server.cs
@@ -5,111 +5,128 @@ using System.Text;
 
 using Driver;
 
-namespace Server {
-   class TelemetryServer {
-      const int BUFFER_SIZE = 1024;
-      const int MS_DELAY    = 50; // 20 Hz
-      const int CONNECTED_CLIENT_TIMEOUT_MS = 5000;
+namespace Server
+{
+    class TelemetryServer
+    {
+        const int BUFFER_SIZE = 1024;
+        const int MS_DELAY = 50; // 20 Hz
+        const int CONNECTED_CLIENT_TIMEOUT_MS = 5000;
 
-      const int DEFAULT_PORT = 9000;
+        const int DEFAULT_PORT = 9000;
 
-      private int port = DEFAULT_PORT;
-      private IPAddress broadcast_address = IPAddress.Any;
-      private Socket broadcaster;
-      private IPEndPoint local_endpoint;
+        readonly int port = DEFAULT_PORT;
+        readonly IPAddress broadcast_address = IPAddress.Any;
+        readonly Socket broadcaster;
+        readonly IPEndPoint local_endpoint;
 
-      private SocketAsyncEventArgs connectionListnerArgs;
-      private byte[] connectionListenerBuffer;
+        private SocketAsyncEventArgs connectionListnerArgs;
+        private byte[] connectionListenerBuffer;
 
-      readonly TelemetryParser telemetry_source;
+        readonly TelemetryParser telemetry_source;
 
-      private Dictionary<EndPoint, long> connected_clients = new Dictionary<EndPoint, long>();
+        readonly Dictionary<EndPoint, long> connected_clients = new();
 
-      public TelemetryServer(string[] args) {
-         telemetry_source = new TelemetryParser();
+        public TelemetryServer(string[] args)
+        {
+            telemetry_source = new TelemetryParser();
 
-         if (args.Length >= 2) {
-            this.broadcast_address = IPAddress.Parse(args[0]);
-            this.port = Int32.Parse(args[1]);
-         }
-         
-         this.broadcaster  = new Socket(this.broadcast_address.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
-         this.local_endpoint = new IPEndPoint(this.broadcast_address, this.port);
-
-         connectionListenerBuffer = new byte[BUFFER_SIZE];
-         this.connectionListnerArgs = BuildConnectionListener();
-      }
-
-      ~TelemetryServer() {
-         broadcaster.Dispose();
-         connectionListnerArgs.Dispose();
-      }
-
-      public void ExecuteUDPServer() {
-         Console.WriteLine($"Beginning UDP broadcast on: {local_endpoint}");
-
-         this.broadcaster.Bind(this.local_endpoint);
-         
-         this.broadcaster.ReceiveFromAsync(connectionListnerArgs);
-
-         while(true) {
-            if(connected_clients.Count == 0) {
-               Thread.Sleep(MS_DELAY);
+            if (args.Length >= 2)
+            {
+                this.broadcast_address = IPAddress.Parse(args[0]);
+                this.port = Int32.Parse(args[1]);
             }
-            else {
-               byte[] sendbuf = Encoding.ASCII.GetBytes(this.telemetry_source.GetJSONTelemetryData());
-               long start_time_millis = CurrentTimeMillis();
 
-               foreach ((EndPoint client, long lastHeartbeatMillis) in connected_clients) {
-                  if(lastHeartbeatMillis < (start_time_millis - CONNECTED_CLIENT_TIMEOUT_MS)) {
-                     Console.WriteLine($"Heartbeat timeout for {client} after {(start_time_millis - CONNECTED_CLIENT_TIMEOUT_MS)}ms");
-                     connected_clients.Remove(client);
-                     continue;
-                  }
-                  Console.WriteLine($"Sending to {client}");
-                  this.broadcaster.SendTo(sendbuf, client);
-               }
+            this.broadcaster = new Socket(this.broadcast_address.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+            this.local_endpoint = new IPEndPoint(this.broadcast_address, this.port);
 
-               Console.WriteLine($"Sleeping for {Math.Max((start_time_millis + MS_DELAY - CurrentTimeMillis()), 0)}ms");
-               Thread.Sleep((int)Math.Max((start_time_millis + MS_DELAY - CurrentTimeMillis()), 0));
+            connectionListenerBuffer = new byte[BUFFER_SIZE];
+            this.connectionListnerArgs = BuildConnectionListener();
+        }
+
+        ~TelemetryServer()
+        {
+            broadcaster.Dispose();
+            connectionListnerArgs.Dispose();
+        }
+
+        public void ExecuteUDPServer()
+        {
+            Console.WriteLine($"Beginning UDP broadcast on: {local_endpoint}");
+
+            this.broadcaster.Bind(this.local_endpoint);
+
+            this.broadcaster.ReceiveFromAsync(connectionListnerArgs);
+
+            while (true)
+            {
+                if (connected_clients.Count == 0)
+                {
+                    Thread.Sleep(MS_DELAY);
+                }
+                else
+                {
+                    byte[] sendbuf = Encoding.ASCII.GetBytes(this.telemetry_source.GetJSONTelemetryData());
+                    long start_time_millis = CurrentTimeMillis();
+
+                    foreach ((EndPoint client, long lastHeartbeatMillis) in connected_clients)
+                    {
+                        if (lastHeartbeatMillis < (start_time_millis - CONNECTED_CLIENT_TIMEOUT_MS))
+                        {
+                            Console.WriteLine($"Heartbeat timeout for {client} after {start_time_millis - CONNECTED_CLIENT_TIMEOUT_MS}ms");
+                            connected_clients.Remove(client);
+                            continue;
+                        }
+                        Console.WriteLine($"Sending to {client}");
+                        this.broadcaster.SendTo(sendbuf, client);
+                    }
+
+                    Console.WriteLine($"Sleeping for {Math.Max(start_time_millis + MS_DELAY - CurrentTimeMillis(), 0)}ms");
+                    Thread.Sleep((int)Math.Max(start_time_millis + MS_DELAY - CurrentTimeMillis(), 0));
+                }
             }
-         }
-      }
+        }
 
-      private SocketAsyncEventArgs BuildConnectionListener() {
-         SocketAsyncEventArgs newConnectionListner = new SocketAsyncEventArgs();
-         connectionListenerBuffer = new byte[BUFFER_SIZE];
+        private SocketAsyncEventArgs BuildConnectionListener()
+        {
+            SocketAsyncEventArgs newConnectionListner = new();
+            connectionListenerBuffer = new byte[BUFFER_SIZE];
 
-         newConnectionListner.Completed += new EventHandler<SocketAsyncEventArgs>(ConnectionHandler);
-         newConnectionListner.SetBuffer(connectionListenerBuffer, 0, BUFFER_SIZE);
-         newConnectionListner.RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            newConnectionListner.Completed += new(ConnectionHandler);
+            newConnectionListner.SetBuffer(connectionListenerBuffer, 0, BUFFER_SIZE);
+            newConnectionListner.RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
-         return newConnectionListner;
-      }
+            return newConnectionListner;
+        }
 
-      void ConnectionHandler(object? sender, SocketAsyncEventArgs e) {
-         long connection_request_time_ms = CurrentTimeMillis();
+        void ConnectionHandler(object? sender, SocketAsyncEventArgs e)
+        {
+            long connection_request_time_ms = CurrentTimeMillis();
 
-         if(e.Buffer == null || e.RemoteEndPoint == null) {
-            throw new NullReferenceException();
-         }
-         Console.WriteLine($"Received '{Encoding.ASCII.GetString(e.Buffer)}' from {e.RemoteEndPoint} at {connection_request_time_ms}ms");
+            if (e.Buffer == null || e.RemoteEndPoint == null)
+            {
+                throw new NullReferenceException();
+            }
+            Console.WriteLine($"Received '{Encoding.ASCII.GetString(e.Buffer)}' from {e.RemoteEndPoint} at {connection_request_time_ms}ms");
 
-         if(e.SocketError != SocketError.Success){
-            // A host is no longer connected. Will be removed from connected_clients after heartbeat timeout.
-           Console.WriteLine($"Error {e.SocketError}");
-         }
-         else {
-            Console.WriteLine($"Adding {e.RemoteEndPoint}");
-            connected_clients[e.RemoteEndPoint] = connection_request_time_ms;
-         }
-         e.Dispose();
-         this.connectionListnerArgs = BuildConnectionListener();
-         this.broadcaster.ReceiveFromAsync(connectionListnerArgs);
-      }
+            if (e.SocketError != SocketError.Success)
+            {
+                // A host is no longer connected. Will be removed from connected_clients after heartbeat timeout.
+                Console.WriteLine($"Error {e.SocketError}");
+            }
+            else
+            {
+                Console.WriteLine($"Adding {e.RemoteEndPoint}");
+                connected_clients[e.RemoteEndPoint] = connection_request_time_ms;
+            }
+            e.Dispose();
+            this.connectionListnerArgs = BuildConnectionListener();
+            this.broadcaster.ReceiveFromAsync(connectionListnerArgs);
+        }
 
-      private static long CurrentTimeMillis() {
-         return DateTime.Now.Ticks/TimeSpan.TicksPerMillisecond;
-      }
-   }
+        private static long CurrentTimeMillis()
+        {
+            return DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        }
+    }
 }
